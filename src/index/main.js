a274d888447e9933e5f1f08b9c870a5ccbe28d29
kazuma1989 <funifuni.1204@gmail.com>
2018-01-18 23:36:19 +0000

    Added IE8 HTML5 support

--- src/index/main.js
+++ src/index/main.js
@@ -1,7 +1,5 @@
 import ko from 'knockout';
-import $ from 'jquery';
 import AppViewModel from '../common/AppViewModel';
-import style from './style.css';
 import icon from './icon.png';
 
 const app = new AppViewModel();
@@ -10,5 +8,3 @@ const app = new AppViewModel();
 ko.applyBindings(app);
 
 app.filename(icon);
-
-$('body').addClass(style.bgred);
